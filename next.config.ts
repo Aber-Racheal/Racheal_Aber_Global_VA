ce517df8e1ad9f1f7d96793f958b747c5fa75368
Aber-Racheal <rachealaberr@gmail.com>
2026-01-19 20:40:04 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,31 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/Pixelize" : "";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath,
-  assetPrefix: basePath,
   images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
+    unoptimized: true, // optional, if you don’t want Next.js image optimization
   },
 };
 
 export default nextConfig;
+
+
+
+// import type { NextConfig } from "next";
+
+// const isProd = process.env.NODE_ENV === "production";
+// const basePath = isProd ? "/Pixelize" : "";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   basePath,
+//   assetPrefix: basePath,
+//   images: {
+//     unoptimized: true,
+//   },
+//   trailingSlash: true,
+//   env: {
+//     NEXT_PUBLIC_BASE_PATH: basePath,
+//   },
+// };
+
+// export default nextConfig;
